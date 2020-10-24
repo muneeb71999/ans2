639b02b19775938e6400c0d16f7a238cfad883a2
muneeb71999 <muneebakram71999@gmail.com>
2020-10-24 10:50:24 +0500

    Intial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./**.html"],
+  },
+  theme: {
+    borderRadius: {
+      sm: "0.125rem",
+      default: "0.25rem",
+      default: "4px",
+      md: "0.375rem",
+      lg: "0.5rem",
+      full: "9999px",
+      large: "12px",
+      xl: "13.25px",
+    },
+    extend: {
+      colors: {
+        "orange-400": "#FF9900",
+        "red-500": "#FF4F4F",
+      },
+    },
+  },
+};
