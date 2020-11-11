2fac70542e46754b77e159473d888b6acaf8b36c
muneeb71999 <muneebakram71999@gmail.com>
2020-11-11 20:01:41 +0500

    Intial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
     extend: {
       colors: {
         "orange-400": "#FF9900",
-        "red-500": "#FF4F4F",
+        "red-500": "#5a67d8",
       },
     },
   },
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const OptimizCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   optimization: {
     minimizer: [new OptimizCssAssetsPlugin(), new TerserPlugin()],
   },
